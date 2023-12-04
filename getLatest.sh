cfd29781f8a7ab60be29e6c372f8f45324fe3a9e
George Bell <4380159+georgevicbell@users.noreply.github.com>
2023-12-04 17:32:13 -0500

    first commit

---
+++ getLatest.sh
@@ -0,0 +1,7 @@
+# This script will get the latest version of the bylaws from the city of toronto's website
+
+mkdir pdfs
+
+for i in $(seq -w 1 999);do
+    curl -L -f http://www.toronto.ca/legdocs/municode/1184_$i.pdf --output pdfs/$i.pdf
+done
